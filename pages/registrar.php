dc32afaad1c7a370ddd44dc424fbf168f0fc6dd9
Jorge de J. Pérez López <jodeperezlo@ittepic.edu.mx>
2020-11-27 02:15:12 -0600

    REGISTRAR IMAGENES EN LA BD
    Agregué los archivos necesarios para poder registrar imágenes en la base de datos llamada 'aspirantestec', en la tabla llamada 'imagenes'.

---
+++ pages/registrar.php
@@ -0,0 +1,27 @@
+<?php
+    include('includes/encabezado.php');
+    include('includes/utilerias.php');
+?>
+   <!--Div que engloba el área para registrar una nueva imágen.-->
+   <div class="contenedor-admin">                
+        <div class="datos" id="modificable">
+            <form action="guardar.php" id="info" method="post" enctype="multipart/form-data">
+                <label class = "etiqueta">Título de la imagen:</label>
+                <input type="text" id="nombre" class="text-input" name="nombre" placeholder="Nombre..." requiere>
+                
+                <label class = "etiqueta">Descripción de la imagen:</label>
+                <input type="text" id="descripcion" class="text-input" name="descripcion" placeholder="Descripción..." requiere>
+                
+                <label class = "etiqueta">Cargar una imagen:</label>
+                <input type="file" id="imagenfile" name="imagen" class="etiqueta" accept = ".jpg, .gif, .png">
+                
+                <div class="botones">
+                    <input type="submit" value="Guardar Imagen" id = "boton">
+                    <input type="reset" value="Borrar Datos" id = "boton">
+                </div>
+            </form>
+        </div>      
+    </div> <!-- Termina el div del admin -->
+    <?php
+    include('includes/pie.php');
+    ?>
