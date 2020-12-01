32a4076e3e8293f038042b7452d3d2d3da1a8d64
raramirezhe <raramirezhe@ittepic.edu.mx>
2020-12-01 10:57:46 -0700

    Cambios en DeInteres

--- pages/deinteres.php
+++ pages/deinteres.php
@@ -10,14 +10,14 @@
         <!--Inicia el article de CISCO -->
         <article class="tarjeta-interes">
             <img src = "../img/deinteres/CISCO.png" alt ="Imagen CISCO">
-            <h3 id="cisco">CERTIFICACIÓN CISCO</h3>	  
+            <h3 id="cisco">CERTIFICACIÓN</h3>	  
             <p> 
                El Instituto Tecnológico de Tepic, tiene una asoaciación directa con Cisco Networking Academy,
                 ofreciendo de esta manera, poder obtener 3 de las 4 certificaciones que CISCO ofrece.
                 Ofreciendo así al estudiante, poder tener un plus en su currículum, y tener una formación
                 integral.
             </p>
-            <a href="https://www.netacad.com/">Visitar CISCO</a>
+            <a href="https://www.netacad.com/" target="_blank">Visitar CISCO</a>
         </article><!--Termina el article de CISCO -->
     
         <!--Inicia el article de SolidWorks -->
@@ -28,7 +28,7 @@
                El Instituto Tecnológico de Tepic, ofrece a través de un curso de formación, la certificación
                en el programa SolidWorks. SolidWorks es un software CAD para modelado mecánico en 2D y 3D, desarrollado en la actualidad por SolidWorks Corp., una filial de Dassault Systèmes, S.A., para el sistema operativo Microsoft Windows.
             </p>
-            <a href="https://www.solidworks.com/es">Visitar SOLIDWORKS</a>
+            <a href="https://www.solidworks.com/es" target="_blank">Visitar SOLIDWORKS</a>
         </article><!--Termina el article de SolidWorks -->
 
         <!--Inicia el article de Beca Jóvenes -->
@@ -39,7 +39,7 @@
                El Instituto Tecnológico de Tepic, permite al estudiante poder solicitar la beca "Jóvenes Escribiendo el Futuro",
                de esta manera, aprovechamos el apoyo del gobierno, y damos la oportunidad al estudiante de tener un ingreso que se entrega directamente al estudiante mediante una tarjeta bancaria.
             </p>
-            <a href="https://www.gob.mx/becasbenitojuarez/articulos/beca-jovenes-escribiendo-el-futuro-de-educacion-superior">Visitar BECA JÓVENES ESCRIBIENDO EL FUTURO</a>
+            <a href="https://www.gob.mx/becasbenitojuarez/articulos/beca-jovenes-escribiendo-el-futuro-de-educacion-superior" target="_blank">Visitar BECA JÓVENES ESCRIBIENDO EL FUTURO</a>
         </article><!--Termina el article de Beca Jóvenes -->
 
          <!--Inicia el article de Beca Jóvenes -->
@@ -50,7 +50,7 @@
                El Instituto Tecnológico de Tepic, permite al estudiante poder solicitar la beca "Jóvenes Escribiendo el Futuro",
                de esta manera, aprovechamos el apoyo del gobierno, y damos la oportunidad al estudiante de tener un ingreso que se entrega directamente al estudiante mediante una tarjeta bancaria.
             </p>
-            <a href="https://www.gob.mx/becasbenitojuarez/articulos/beca-jovenes-escribiendo-el-futuro-de-educacion-superior">Visitar BECA JÓVENES ESCRIBIENDO EL FUTURO</a>
+            <a href="https://www.gob.mx/becasbenitojuarez/articulos/beca-jovenes-escribiendo-el-futuro-de-educacion-superior" target="_blank">Visitar BECA JÓVENES ESCRIBIENDO EL FUTURO</a>
         </article><!--Termina el article de Beca Jóvenes -->
 
          <!--Inicia el article de Beca Jóvenes -->
@@ -61,7 +61,32 @@
                El Instituto Tecnológico de Tepic, permite al estudiante poder solicitar la beca "Jóvenes Escribiendo el Futuro",
                de esta manera, aprovechamos el apoyo del gobierno, y damos la oportunidad al estudiante de tener un ingreso que se entrega directamente al estudiante mediante una tarjeta bancaria.
             </p>
-            <a href="https://www.gob.mx/becasbenitojuarez/articulos/beca-jovenes-escribiendo-el-futuro-de-educacion-superior">Visitar BECA JÓVENES ESCRIBIENDO EL FUTURO</a>
+            <a href="https://www.gob.mx/becasbenitojuarez/articulos/beca-jovenes-escribiendo-el-futuro-de-educacion-superior" target="_blank">Visitar BECA JÓVENES ESCRIBIENDO EL FUTURO</a>
+        </article><!--Termina el article de Beca Jóvenes -->
+
+        <!--Inicia el article de Beca CONACYT -->
+        <article class="tarjeta-interes">
+            <img src = "../img/deinteres/CONACYT.png" alt ="Imagen Beca Jóvenes">
+            <h3 id="cisco">Becas CONACYT</h3>	  
+            <p> 
+            El Consejo Nacional de Ciencia y Tecnología formula y financia programas de becas y, en general, 
+            brinda apoyo para la formación de investigadores y científicos en sus diversas modalidades, así como 
+            también integra la información de los programas de becas que ofrezcan otras instituciones nacionales...
+            </p>
+            <a href="https://www.gob.mx/becasbenitojuarez/articulos/beca-jovenes-escribiendo-el-futuro-de-educacion-superior" target="_blank">Visitar BECAS CONACYT</a>
+        </article><!--Termina el article de Beca Jóvenes -->
+
+        <!--Inicia el article de Beca Ingles -->
+        <article class="tarjeta-interes">
+            <img src = "../img/deinteres/ingles.png" alt ="Imagen Beca Jóvenes">
+            <h3 id="cisco">Becas para estudiar Inglés</h3>	  
+            <p> 
+            A los estudiantes y docentes de Instituciones Públicas de Educación Superior (IPES)
+            del país a solicitar una BECA DE CAPACITACIÓN para cursar estudios intensivos del
+            idioma inglés como segunda lengua en centros certificados y pertenecientes a una
+            institución de educación superior en los Estados Unidos de América.
+            </p>
+            <a href="https://www.gob.mx/becasbenitojuarez/articulos/beca-jovenes-escribiendo-el-futuro-de-educacion-superior" target="_blank">Visitar BECAS PARA INGLÉS</a>
         </article><!--Termina el article de Beca Jóvenes -->
     
     </div><!-- DIV para el contenedor de tarjetas. -->
