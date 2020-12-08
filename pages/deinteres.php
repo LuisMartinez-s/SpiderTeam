acda72e52252687838551822cbabb9c2a1ac0bec
raramirezhe <raramirezhe@ittepic.edu.mx>
2020-12-08 15:50:58 -0700

    Rediseño becasYCertif

--- pages/deinteres.php
+++ pages/deinteres.php
@@ -19,66 +19,120 @@
 </div>
 
 <div class = "contenedor-principal-interes">
-    <!-- Div para el contenedor de las tarjetas.-->
+<h1 class="titulos" id="titulos">Becas</h1>
+    <!-- Div para el contenedor de las tarjetas BECAS.-->
     <div class="contenedor-tarjetas-interes">
         
         <!--Inicia el article de CISCO -->
         <article class="tarjeta-interes">
-            <img src = "../img/deinteres/CISCO.jpg" alt ="Imagen CISCO">
-            <h3 id="nombre">CERTIFICACIÓN CISCO</h3>	  
-            <a id="enlace" target = "_blank" href="https://www.netacad.com/">Ver mas...</a>
+            <img src = "../img/deinteres/IMG.png" alt ="">
+            <h3 id="nombre">Beca #</h3>	  
+            <a id="enlace" target = "_blank" href="">Ver mas...</a>
         </article><!--Termina el article de CISCO -->
     
         <!--Inicia el article de SolidWorks -->
         <article class="tarjeta-interes">
-            <img src = "../img/deinteres/solidworks.jpg" alt ="Imagen SolidWorks">
-            <h3 id="nombre">CERTIFICACIÓN SOLIDWORKS</h3>	  
-            <a id="enlace" target = "_blank" href="https://www.solidworks.com/es">Ver mas...</a>
+            <img src = "../img/deinteres/IMG.png" alt ="">
+            <h3 id="nombre">Beca #</h3>	  
+            <a id="enlace" target = "_blank" href="">Ver mas...</a>
         </article><!--Termina el article de SolidWorks -->
 
         <!--Inicia el article de Beca Jóvenes -->
         <article class="tarjeta-interes">
-            <img src = "../img/deinteres/jovenes-escribiendo-el-futuro.jpg" alt ="Imagen Beca Jóvenes">
-            <h3 id="nombre">BECA JÓVENES ESCRIBIENDO EL FUTURO</h3>	  
-            <a id="enlace" target = "_blank" href="https://www.gob.mx/becasbenitojuarez/articulos/beca-jovenes-escribiendo-el-futuro-de-educacion-superior">Ver mas...</a>
+            <img src = "../img/deinteres/IMG.png" alt ="">
+            <h3 id="nombre">Beca #</h3>	  
+            <a id="enlace" target = "_blank" href="">Ver mas...</a>
         </article><!--Termina el article de Beca Jóvenes -->
 
          <!--Inicia el article de Beca Jóvenes -->
          <article class="tarjeta-interes">
-            <img src = "../img/deinteres/cnbes.jpg" alt ="Imagen Beca Jóvenes">
-            <h3 id="nombre">BECA JÓVENES ESCRIBIENDO EL FUTURO</h3>	  
-            <a id="enlace" target = "_blank" href="https://www.gob.mx/becasbenitojuarez/articulos/beca-jovenes-escribiendo-el-futuro-de-educacion-superior">Ver mas...</a>
+            <img src = "../img/deinteres/IMG.png" alt ="">
+            <h3 id="nombre">Beca #</h3>	  
+            <a id="enlace" target = "_blank" href="">Ver mas...</a>
         </article><!--Termina el article de Beca Jóvenes -->
 
          <!--Inicia el article de Beca Jóvenes -->
          <article class="tarjeta-interes">
-            <img src = "../img/deinteres/Conacyt.png" alt ="Imagen Beca Jóvenes">
-            <h3 id="nombre">BECA JÓVENES ESCRIBIENDO EL FUTURO</h3>	  
-            <a id="enlace" target = "_blank" href="https://www.gob.mx/becasbenitojuarez/articulos/beca-jovenes-escribiendo-el-futuro-de-educacion-superior">Ver mas...</a>
+            <img src = "../img/deinteres/IMG.png" alt ="">
+            <h3 id="nombre">Beca #</h3>	  
+            <a id="enlace" target = "_blank" href="">Ver mas...</a>
         </article><!--Termina el article de Beca Jóvenes -->
     
 
         <!--Inicia el article de Beca Jóvenes -->
         <article class="tarjeta-interes">
-            <img src = "../img/deinteres/ingles.jpeg" alt ="Imagen Beca Jóvenes">
-            <h3 id="nombre">BECA JÓVENES ESCRIBIENDO EL FUTURO</h3>	  
-            <a id="enlace" target = "_blank" href="https://www.gob.mx/becasbenitojuarez/articulos/beca-jovenes-escribiendo-el-futuro-de-educacion-superior">Ver mas...</a>
+            <img src = "../img/deinteres/IMG.png" alt ="">
+            <h3 id="nombre">Beca #</h3>	  
+            <a id="enlace" target = "_blank" href="">Ver mas...</a>
         </article><!--Termina el article de Beca Jóvenes -->
 
         <!--Inicia el article de Beca Jóvenes -->
         <article class="tarjeta-interes">
-            <img src = "../img/deinteres/excelencia.png" alt ="Imagen Beca Jóvenes">
-            <h3 id="nombre">BECA JÓVENES ESCRIBIENDO EL FUTURO</h3>	  
-            <a id="enlace" target = "_blank" href="https://www.gob.mx/becasbenitojuarez/articulos/beca-jovenes-escribiendo-el-futuro-de-educacion-superior">Ver mas...</a>
+            <img src = "../img/deinteres/IMG.png" alt ="">
+            <h3 id="nombre">Beca #</h3>	  
+            <a id="enlace" target = "_blank" href="">Ver mas...</a>
         </article><!--Termina el article de Beca Jóvenes -->
 
         <!--Inicia el article de Beca Jóvenes -->
         <article class="tarjeta-interes">
-            <img src = "../img/deinteres/deporte.png" alt ="Imagen Beca Jóvenes">
-            <h3 id="nombre">BECA JÓVENES ESCRIBIENDO EL FUTURO</h3>	  
-            <a id="enlace" target = "_blank" href="https://www.gob.mx/becasbenitojuarez/articulos/beca-jovenes-escribiendo-el-futuro-de-educacion-superior">Ver mas...</a>
+            <img src = "../img/deinteres/IMG.png" alt ="">
+            <h3 id="nombre">Beca #</h3>	  
+            <a id="enlace" target = "_blank" href="">Ver mas...</a>
         </article><!--Termina el article de Beca Jóvenes -->        
     </div><!-- DIV para el contenedor de tarjetas. -->
+
+    <h1 class="titulos" id="titulos">Certificaciones</h1>
+    <!-- DIV PARA CERTIFICACIONES -->
+    <div class="contenedor-tarjetas-interes">
+
+        <article class="tarjeta-interes">
+            <img src="../img/deinteres/IMG.png" alt="">
+            <h3 id="nombre">Certificación #</h3>
+            <a href="" id="enlace" target="_blank">Ver mas...</a>
+        </article>
+
+        <article class="tarjeta-interes">
+            <img src="../img/deinteres/IMG.png" alt="">
+            <h3 id="nombre">Certificación #</h3>
+            <a href="" id="enlace" target="_blank">Ver mas...</a>
+        </article>
+
+        <article class="tarjeta-interes">
+            <img src="../img/deinteres/IMG.png" alt="">
+            <h3 id="nombre">Certificación #</h3>
+            <a href="" id="enlace" target="_blank">Ver mas...</a>
+        </article>
+
+        <article class="tarjeta-interes">
+            <img src="../img/deinteres/IMG.png" alt="">
+            <h3 id="nombre">Certificación #</h3>
+            <a href="" id="enlace" target="_blank">Ver mas...</a>
+        </article>
+
+        <article class="tarjeta-interes">
+            <img src="../img/deinteres/IMG.png" alt="">
+            <h3 id="nombre">Certificación #</h3>
+            <a href="" id="enlace" target="_blank">Ver mas...</a>
+        </article>
+
+        <article class="tarjeta-interes">
+            <img src="../img/deinteres/IMG.png" alt="">
+            <h3 id="nombre">Certificación #</h3>
+            <a href="" id="enlace" target="_blank">Ver mas...</a>
+        </article>
+
+        <article class="tarjeta-interes">
+            <img src="../img/deinteres/IMG.png" alt="">
+            <h3 id="nombre">Certificación #</h3>
+            <a href="" id="enlace" target="_blank">Ver mas...</a>
+        </article>
+
+        <article class="tarjeta-interes">
+            <img src="../img/deinteres/IMG.png" alt="">
+            <h3 id="nombre">Certificación #</h3>
+            <a href="" id="enlace" target="_blank">Ver mas...</a>
+        </article>
+    </div>
 </div>
 
 <?php
