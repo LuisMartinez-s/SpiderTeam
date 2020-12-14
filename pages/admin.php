5eef7c151811b95bbe13a95abded5934a00c1512
Luis Martinez <55217420+LuisMartinez-s@users.noreply.github.com>
2020-12-14 15:29:56 -0700

    Se corrigió la sesión en admin.php

--- pages/admin.php
+++ pages/admin.php
@@ -1,23 +1,29 @@
-<?php    
-    include("includes/utilerias.php");
-    
+<?php
+if (session_status() == PHP_SESSION_NONE) {
+    session_start();
+}
+
+if (isset($_SESSION["usuario"])) {
     include('includes/encabezado-admin.php');
-    
+} else {
+    include('includes/encabezado.php');
+}
+
 ?>
-    <!--Div que englobe todo -->
-    <div class="contenedor-admin-tabla">        
-        <link rel = "stylesheet" href = "../css/tabla.css">    
-        <table class = 'table'>
+<!--Div que englobe todo -->
+<div class="contenedor-admin-tabla">
+    <link rel="stylesheet" href="../css/tabla.css">
+    <table class='table'>
         <thead class="thead-dark">
             <th>Título de la imagen</th>
             <th>Descripción</th>
             <th>Imagen</th>
             <th>Acciones</th>
             <?php
-                include('crear_tabla.php');
+            include('crear_tabla.php');
             ?>
-        </table>
-    </div>   
+    </table>
+</div>
 <?php
-    include('includes/pie.php');
+include('includes/pie.php');
 ?>
